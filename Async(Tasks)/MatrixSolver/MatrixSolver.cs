0c3aebd3d24ae662da6de199c63888e363a49505
s7xWn <47253767+s7xWn@users.noreply.github.com>
2020-06-27 14:23:26 +0300

    4and5

---
+++ Async(Tasks)/MatrixSolver/MatrixSolver.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MatrixSolver
+{
+    class MatrixSolver
+    {
+
+        static double[] ReadRow(string input, int row)
+        {
+            StreamReader sr = File.OpenText(input); 
+
+            sr.ReadLine(); 
+            for (int i = 0; i < row; i++)
+                sr.ReadLine();
+
+            string[] data = sr.ReadLine().Split(' ');
+            sr.Close();
+
+            double[] rowData = new double[data.Length];
+            for (int i = 0; i < rowData.Length; i++)
+                rowData[i] = double.Parse(data[i]);
+
+            return rowData;
+        }
+
+        static double[] ReadCol(string input, int col)
+        {
+            StreamReader sr = File.OpenText(input);
+
+            string[] header = sr.ReadLine().Split(' ');
+
+            int rows = int.Parse(header[0]); 
+
+            string[] data = new string[rows];
+
+            for (int i = 0; i < rows; i++)
+
+                data[i] = (sr.ReadLine()).Split(' ')[col];
+
+            sr.Close(); 
+
+            double[] colData = new double[data.Length];
+
+            for (int i = 0; i < colData.Length; i++)
+
+                colData[i] = double.Parse(data[i]);
+
+            return colData;
+        }
+
+        static double Mul(double[] rowData, double[] colData)
+        {
+            double result = 0;
+            for (int i = 0; i < rowData.Length; i++) 
+                result += rowData[i] * colData[i];
+            return result;
+        }
+
+        static async Task<double> MulAsync(string inputLeft, string inputRight, int row, int col)
+        {
+            double[] rowData, colData;
+
+            rowData = await Task.Run(() => ReadRow(inputLeft, row)); 
+
+            colData = await Task.Run(() => ReadCol(inputRight, col));
+
+            return await Task.Run<double>(() => Mul(rowData, colData));
+        }
+
+        public static async void Solve(string inputLeft, string inputRight, string output) 
+        {
+            StreamReader sr = File.OpenText(inputLeft); 
+            string[] input = sr.ReadLine().Split(' '); 
+            int rowsLeft = int.Parse(input[0]);
+            int colsLeft = int.Parse(input[1]);
+            sr.Close();
+
+            sr = File.OpenText(inputRight); 
+            input = sr.ReadLine().Split(' '); 
+            int rowsRight = int.Parse(input[0]);  
+            int colsRight = int.Parse(input[1]);  
+            sr.Close();
+
+            if ((rowsLeft != colsRight) || (colsLeft != rowsRight)) 
+                throw new Exception("Matrix cols and rows does not equal.");
+
+            StreamWriter sw = new StreamWriter(output);
+            for (int i = 0; i < rowsLeft; ++i)
+            {
+                Task<double>[] mulTasks = new Task<double>[colsRight]; 
+                for (int j = 0; j < colsRight; ++j)
+                {
+                    mulTasks[j] = MulAsync(inputLeft, inputRight, i, j); 
+                }
+                
+                double[] results = await Task.WhenAll<double>(mulTasks);
+
+                sw.WriteLine(String.Join(" ", results));
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ Threads/MasSort/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Threading;
+
+namespace MergeSortThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = Thread.CurrentThread;
+        
+            t.Name = "Main Thread";
+            Console.WriteLine($"Name: {t.Name}");
+            Console.WriteLine($"IsAlive: {t.IsAlive}");
+            Console.WriteLine($"Priority: {t.Priority}");
+            Console.WriteLine($"Status: {t.ThreadState}");
+
+            int[] arr = new int[65];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(100);
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+            Console.WriteLine();
+
+            int n;
+
+            while (true)
+            {
+                Console.Write("Kol-vo potokov(1,2,4,8) = ");
+                int.TryParse(Console.ReadLine(), out n);
+                if (n == 1 || n == 2 || n == 4 || n == 8) break;
+                else Console.WriteLine("err");
+            }
+
+
+            MultithreadedSort(arr, n);
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public static void MultithreadedSort(int[] arr, int threads)
+        {
+            int[][] arrs = new int[threads][];
+
+            Thread[] sorting = new Thread[threads];
+
+            for (int i = 0; i < threads; i++)
+            {
+                arrs[i] = new int[arr.Length / threads];
+
+                Array.Copy(arr, i * (arr.Length / threads), arrs[i], 0, arr.Length / threads); 
+
+                sorting[i] = new Thread(new ParameterizedThreadStart(sort));
+
+                sorting[i].Start(arrs[i]);
+            }
+
+            for (int i = 0; i < threads; i++)
+
+                sorting[i].Join();
+
+            bool norm = false; 
+
+            while (arrs.Length != 1) 
+            {
+                int k = 0;
+                int[][] tmp = new int[arrs.Length / 2][];
+
+                for (int i = 0; i < arrs.Length; i++)
+                {
+                    if (arr.Length % 2 != 0 && !norm) 
+                    {
+                        tmp[k] = new int[arrs[i].Length + arrs[i + 1].Length + 1];
+                        tmp[k][tmp[k].Length - 1] = arr[arr.Length - 1];
+                        norm = true;
+                    }
+                    else
+                    {
+                        tmp[k] = new int[arrs[i].Length + arrs[i + 1].Length]; 
+                    }
+                    Array.Copy(arrs[i], 0, tmp[k], 0, arrs[i].Length); 
+                    try
+                    {
+                        Array.Copy(arrs[i + 1], 0, tmp[k], arrs[i].Length, arrs[i + 1].Length); 
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                    i++;
+                    k++;
+                }
+
+                Thread[] merging = new Thread[tmp.Length];
+
+                for (int j = 0; j < tmp.Length; j++)
+                {
+                    merging[j] = new Thread(new ParameterizedThreadStart(sort));
+
+                    merging[j].Start(tmp[j]);
+                }
+
+                for (int i = 0; i < tmp.Length; i++)
+                    merging[i].Join();
+
+                arrs = tmp; 
+            }
+
+            arrs[0].CopyTo(arr, 0);
+        }
+
+        public static void sort(object arr)
+        {
+            Array.Sort((int[])arr, 0, ((int[])arr).Length);
+        }
+    }
+}
